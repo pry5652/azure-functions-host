8a031f6bd36658a01f99b4dfa39d9e0c67deac01
Anatoli Beliaev <anatolib@microsoft.com>
2020-09-18 21:12:25 -0700

    Set OS to Ubuntu

--- tools/Crank/Agent/VmDeployment/Program.fs
+++ tools/Crank/Agent/VmDeployment/Program.fs
@@ -1,11 +1,11 @@
-open Farmer
+﻿open Farmer
 open Farmer.Builders
 
 let myVm = vm {
     name "functions-crank-linux-test"
     username "Functions"
     vm_size Vm.Standard_E2s_v3
-    operating_system Vm.WindowsServer_2012Datacenter
+    operating_system Vm.UbuntuServer_1804LTS
     os_disk 128 Vm.Premium_LRS
 }
 
