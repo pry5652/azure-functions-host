15e8d17f6a15a8ebee6fcf466cb2497d2049b1da
Anatoli Beliaev <anatolib@microsoft.com>
2020-09-19 23:44:57 -0700

    Move parameter values to deploy.ps1

--- tools/Crank/Agent/VmDeployment/deploy.ps1
+++ tools/Crank/Agent/VmDeployment/deploy.ps1
@@ -10,6 +10,10 @@ param (
     [string]
     $ResourceGroupName,
 
+    [Parameter(Mandatory = $true)]
+    [string]
+    $VmName,
+
     [string]
     $Location = 'West Central US'
 )
@@ -20,4 +24,13 @@ Set-AzContext -Subscription $SubscriptionName | Out-Null
 
 New-AzResourceGroup -Name $ResourceGroupName -Location $Location | Out-Null
 
-New-AzResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateFile .\template.json -TemplateParameterFile .\parameters.json
+New-AzResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateFile .\template.json `
+    -TemplateParameterObject @{
+        vmName = 'functions-crank-test'
+        adminUserName = 'Functions'
+        authenticationType = 'sshPublicKey'
+        adminPasswordOrKey = TODO
+        dnsLabelPrefix = 'functions-crank-test'
+        location = $Location
+        VmSize = 'Standard_E2s_v3'
+    }
