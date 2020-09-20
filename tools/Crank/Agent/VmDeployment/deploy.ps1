c97dfd06596bf5abc8755784833408582d51db62
Anatoli Beliaev <anatolib@microsoft.com>
2020-09-20 00:33:15 -0700

    Get SSH public key from KV

--- tools/Crank/Agent/VmDeployment/deploy.ps1
+++ tools/Crank/Agent/VmDeployment/deploy.ps1
@@ -24,12 +24,16 @@ Set-AzContext -Subscription $SubscriptionName | Out-Null
 
 New-AzResourceGroup -Name $ResourceGroupName -Location $Location | Out-Null
 
+$sshPublicKey = Get-AzKeyVaultSecret -VaultName 'functions-crank-kv' -Name 'LinuxCrankAgentVmSshKey-Public' |
+                    ForEach-Object SecretValue |
+                    ConvertFrom-SecureString -AsPlainText
+
 New-AzResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateFile .\template.json `
     -TemplateParameterObject @{
         vmName = 'functions-crank-test'
         adminUserName = 'Functions'
         authenticationType = 'sshPublicKey'
-        adminPasswordOrKey = TODO
+        adminPasswordOrKey = $sshPublicKey
         dnsLabelPrefix = 'functions-crank-test'
         location = $Location
         VmSize = 'Standard_E2s_v3'
