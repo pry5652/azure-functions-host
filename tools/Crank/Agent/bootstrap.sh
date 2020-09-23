6ec2e52daf7d67ad815c9295140cfe4752ec4047
Anatoli Beliaev <anatolib@microsoft.com>
2020-09-23 00:29:37 -0700

    TEMP: point ot anatolib/crank-agent-setup

--- tools/Crank/Agent/bootstrap.sh
+++ tools/Crank/Agent/bootstrap.sh
@@ -3,6 +3,7 @@
 mkdir /home/Functions/github
 cd /home/Functions/github
 git clone https://github.com/Azure/azure-functions-host.git
+git checkout anatolib/crank-agent-setup
 
 cd azure-functions-host/tools/Crank/Agent
 ./install-powershell.sh
