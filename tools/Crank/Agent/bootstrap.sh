e14c1ed0ec50337fc71adc6e03161b2d192a3c42
Anatoli Beliaev <anatolib@microsoft.com>
2020-09-23 01:00:18 -0700

    Fix paths

--- tools/Crank/Agent/bootstrap.sh
+++ tools/Crank/Agent/bootstrap.sh
@@ -3,8 +3,9 @@
 mkdir /home/Functions/github
 cd /home/Functions/github
 git clone https://github.com/Azure/azure-functions-host.git
+cd azure-functions-host
 git checkout anatolib/crank-agent-setup
 
-cd azure-functions-host/tools/Crank/Agent
+cd tools/Crank/Agent
 ./install-powershell.sh
 ./setup-crank-agent.ps1 -CrankBranch dev
