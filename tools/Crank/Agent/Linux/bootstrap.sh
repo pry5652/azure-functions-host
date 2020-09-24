d8db88371255d126d7afa4b9136f68fd2be4ab53
Anatoli Beliaev <anatolib@microsoft.com>
2020-09-24 13:04:44 -0700

    chmod from bootstrap.sh

--- tools/Crank/Agent/Linux/bootstrap.sh
+++ tools/Crank/Agent/Linux/bootstrap.sh
@@ -7,5 +7,7 @@ cd azure-functions-host
 git checkout anatolib/crank-agent-setup
 
 cd tools/Crank/Agent
+chmod -R +x *.sh
+chmod -R +x *.ps1
 Linux/install-powershell.sh
 sudo -H -u Functions ./setup-crank-agent.ps1 -CrankBranch master
