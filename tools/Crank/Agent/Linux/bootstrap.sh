92da953e729f674ce7c268aa7383b25c2b075fb4
Anatoli Beliaev <anatolib@microsoft.com>
2020-09-27 21:46:08 -0700

    Convert sh and ps1 files to unix format

--- tools/Crank/Agent/Linux/bootstrap.sh
+++ tools/Crank/Agent/Linux/bootstrap.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-git config --global core.autocrlf input
 mkdir /home/Functions/github
 cd /home/Functions/github
 git clone https://github.com/Azure/azure-functions-host.git
@@ -8,8 +7,14 @@ cd azure-functions-host
 git checkout anatolib/crank-linux-container
 
 cd tools/Crank/Agent
-echo Updated bootstrap.sh
 sudo find . -name "*.sh" -exec sudo chmod +x {} \;
 sudo find . -name "*.ps1" -exec sudo chmod +x {} \;
+
+sudo apt update
+sudo apt install dos2unix
+sudo find . -name "*.sh" -exec dos2unix {} \;
+sudo find . -name "*.ps1" -exec dos2unix {} \;
+
 Linux/install-powershell.sh
+
 ./setup-crank-agent-json.ps1 -ParametersJson $1
