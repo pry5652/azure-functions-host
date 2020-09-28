1005ff52af7c67ee9a135da313def9d4cf5a1d13
Anatoli Beliaev <anatolib@microsoft.com>
2020-09-28 00:12:30 -0700

    traces

--- tools/Crank/Agent/Linux/bootstrap.sh
+++ tools/Crank/Agent/Linux/bootstrap.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-echo *** Cloning azure-functions-host repo
+echo --- Cloning azure-functions-host repo
 mkdir /home/Functions/github
 cd /home/Functions/github
 git clone https://github.com/Azure/azure-functions-host.git
 cd azure-functions-host
 git checkout anatolib/crank-linux-container
 
-echo *** Fixing script files format and permissions
+echo --- Fixing script files format and permissions
 cd tools/Crank/Agent
 sudo find . -name "*.sh" -exec sudo chmod +xr {} \;
 sudo find . -name "*.ps1" -exec sudo chmod +xr {} \;
@@ -19,5 +19,5 @@ sudo find . -name "*.ps1" -exec dos2unix {} \;
 
 Linux/install-powershell.sh
 
-echo *** Invoking setup-crank-agent-json.ps1 with parameters: $1
+echo --- Invoking setup-crank-agent-json.ps1 with parameters: $1
 ./setup-crank-agent-json.ps1 -ParametersJson $1
